a933c9c07115a293b982d2b9b005b3f34f482d64
mishachornyy <85844519+mishachornyy@users.noreply.github.com>
2022-09-03 01:14:52 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Menu from "./Comp/Menu";
+
+function App() {
+  return (
+     <> <Menu />
+   <h1>test3</h1></>
+  );
+}
+
+export default App;
---
+++ src/Comp/Menu.js
@@ -0,0 +1,12 @@
+
+
+import './Menu.css';
+
+function Menu() {
+    return (
+        <div class="px-8 bg-green-600">
+        <h1>test1</h1> </div>
+    );
+}
+
+export default Menu;
